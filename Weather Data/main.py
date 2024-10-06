13cb75f2b60267307433375aec7efc2bdca3d9b2
Omed Hossaini <144287920+OmedHossaini@users.noreply.github.com>
2024-10-06 19:10:50 -0400

    Add files via upload
    imported all projects from the last couple of days

---
+++ Weather Data/main.py
@@ -0,0 +1,27 @@
+# import csv
+# import pandas
+#
+# # with open("weather_data.csv") as data_file:
+# #     data = csv.reader(data_file)
+# #     for row in data:
+# #         temperatures = []
+# #         if row[1] != "temp":
+# #             temperatures.append(int(row[1]))
+# #         print(temperatures)
+#
+# data = pandas.read_csv("weather_data.csv")
+# type
+# print(data)
+
+# import pandas
+#
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+# print(grey_squirrels)
+
+import turtle
+
+
+
