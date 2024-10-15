45592d7714549d11a42d6d136d406ca8b933e590
OmedHossaini <omedhossainiomedhossaini123@hotmail.com>
2024-10-14 22:19:01 -0400

    circle

--- Turtle Racing/main.py
+++ Turtle Racing/main.py
@@ -11,7 +11,7 @@ all_turtles = []
 
 #Create 6 turtles
 for turtle_index in range(0, 6):
-    new_turtle = Turtle(shape="turtle")
+    new_turtle = Turtle(shape="circle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
     new_turtle.goto(x=-230, y=y_positions[turtle_index])
